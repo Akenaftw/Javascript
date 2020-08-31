352a58348652f1306e5599edf3569cc6f25e1554
Akena <felix.j.c.vanroost.com>
2020-08-31 15:05:51 +0200

    finished exercise 8 and 9

--- js/js-complete-course/06-dom/08-generate-table-two/script.js
+++ js/js-complete-course/06-dom/08-generate-table-two/script.js
@@ -9,8 +9,37 @@
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
 
-(function() {
+(function () {
+    function tablemaker() {
+        var target = document.getElementById("target");
+        var table = document.createElement("table");
+        var totalrows = 10;
+        var totalcol = 10;
 
-    // your code here
+        for (var r = 0; r < totalrows; r++) {
+            var row = document.createElement("tr");
 
+
+            for (var c = 0; c < totalcol; c++) {
+                var collumn = document.createElement("td");
+                row.appendChild(collumn);
+            }
+            table.appendChild(row);
+        }
+        target.appendChild(table);
+    }
+
+    tablemaker();
+    var tableshaker = document.getElementsByTagName("table");
+    for (x = 0; x < 10; x++) {
+        for (let y = 0; y < 10; y++) {
+            let c = tableshaker[0].rows[y].cells;
+            let d = y + 1;
+            let a = x + 1;
+            let b = d * a;
+            c[x].innerHTML = b;
+
+        }
+
+    }
 })();
--- js/js-complete-course/06-dom/09-match-password-one/script.js
+++ js/js-complete-course/06-dom/09-match-password-one/script.js
@@ -9,8 +9,19 @@
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
 
-(function() {
+(function () {
+    document.getElementById("run").addEventListener("click", function () {
+        var password1 = document.getElementById("pass-one");
+        var password2 = document.getElementById("pass-two");
 
-    // your code here
+        if (password1.value === password2.value)
+        {
+            alert("Password succesfully set.");
+        }
+        else {
+            password1.style.border = "2px solid red"
+            password2.style.border = "2px solid red"
+        }
 
+    })
 })();
