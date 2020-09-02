e86c01398aeb451f381b68ce4462c824fddf71f7
Akena <felix.j.c.vanroost.com>
2020-09-02 15:55:46 +0200

    finished 7.4

--- js/js-complete-course/07-misc/04-worst-ui-two/script.js
+++ js/js-complete-course/07-misc/04-worst-ui-two/script.js
@@ -9,8 +9,75 @@
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
 
-(function() {
+(function () {
+    let buttonOne = document.getElementById("part-one");
+    let buttonTwo = document.getElementById("part-two");
+    let buttonThree = document.getElementById("part-three");
+    let buttonFour = document.getElementById("part-four");
+    let value = 0;
 
-    // your code here
+    function update() {
+        document.getElementById("target").innerHTML = "0" + buttonOne.innerHTML + buttonTwo.innerHTML + buttonThree.innerHTML + buttonFour.innerHTML;
+    }
 
-})();
+    buttonOne.onclick = function () {
+        value = parseInt(buttonOne.innerHTML);
+        if (value < buttonOne.dataset.max) {
+            value++;
+            buttonOne.innerHTML = value;
+        } else {
+            buttonOne.innerHTML = buttonOne.dataset.min;
+        }
+        update()
+    }
+
+    buttonTwo.onclick = function () {
+        value = parseInt(buttonTwo.innerHTML);
+        if (value < buttonTwo.dataset.max) {
+            value++;
+            if (value < 10) {
+                buttonTwo.innerHTML = "0" + value
+            } else {
+                value = value
+            }
+        } else {
+            buttonTwo.innerHTML = buttonTwo.dataset.min;
+        }
+        update()
+    }
+
+    buttonThree.onclick = function () {
+        value = parseInt(buttonThree.innerHTML);
+        if (value < buttonThree.dataset.max) {
+            value++;
+            if (value < 10) {
+                buttonThree.innerHTML = "0" + value
+            } else {
+                value = value
+            }}
+        else
+            {
+                buttonThree.innerHTML = buttonThree.dataset.min;
+            }
+            update()
+        }
+
+        buttonFour.onclick = function () {
+            value = parseInt(buttonFour.innerHTML);
+            if (value < buttonFour.dataset.max) {
+                value++;
+                if (value < 10) {
+                    buttonFour.innerHTML = "0" + value
+                } else {
+                    value = value
+                }}
+            else
+                {
+                    buttonFour.innerHTML = buttonFour.dataset.min;
+                }
+                update()
+            }
+
+
+        }
+    )();
