8628c875675e6c864abae9246d98c0e2ccd8b978
Akena <felix.j.c.vanroost.com>
2020-09-07 16:28:13 +0200

    finished exc. 8.3 async

--- js/js-complete-course/08-asynchronous/03-handling-errors/script.js
+++ js/js-complete-course/08-asynchronous/03-handling-errors/script.js
@@ -1,14 +1,13 @@
-/* becode/javascript
- *
- * /10-asynchronous/03-handling-errors/script.js - 10.3: gestion d'erreur
- *
- * coded by leny@BeCode
- * started at 09/05/2019
- */
 
-// NOTE: don't focus on the existing code structure for now.
-// You will have time to focus on it later.
+document.getElementById("run").addEventListener("click",getPersons)
+function getPersons() {
+    window.lib.getPosts(function (error, response) {
+        if (error) {
+            console.error("kapoet")
+        } else (response)
+        {
+            console.log(response)
+        }
+    })
 
-(() => {
-    // your code here
-})();
+};
